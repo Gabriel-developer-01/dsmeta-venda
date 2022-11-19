27f35adef83831ac52058b559f074ede6e1c6ca9
Gabriel-Developer-01 <dev_gabrielsouza@hotmail.com>
2022-11-19 18:49:35 -0300

    changing link

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por
-          <a href="{https://www.instagram.com/devsuperior.ig}">@devsuperior.ig</a>
+          <a href="https://www.linkedin.com/in/gabriel-mendes-3a668917b/" target="_blank"> Gabriel Mendes</a>
         </p>
       </div>
     </header>
